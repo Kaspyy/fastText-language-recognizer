c757a8bc9911a24239c9f16fbe68d23d9ee142bf
Kacper <kpawlak2000@gmail.com>
2022-12-12 18:44:40 +0100

    add threshold

--- src/app.py
+++ src/app.py
@@ -1,8 +1,14 @@
 import fasttext
 import fasttext.util
+import os
 
-# train model
-model = fasttext.train_supervised(input="data/dataset.txt")
+# train model if not already trained
+if not os.path.exists("model/model.bin"):
+    model = fasttext.train_supervised(input="data/dataset.txt")
+    model.save_model("model/model.bin")
+
+# load model
+model = fasttext.load_model("model/model.bin")
 
 while True:
     # Prompt the user for input and use the model to predict the language
@@ -14,7 +20,7 @@ while True:
         break
 
     # Use the model to predict the language of the input
-    predicted_language = model.predict(user_input)[0][0]
+    predicted_language = model.predict(user_input, threshold=0.6)[0][0]
 
     # Remove the "__label__" prefix from the predicted language
     predicted_language = predicted_language.replace("__label__", "")
